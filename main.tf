fa9ed155b0a63c42f57884365a0daef178c0b1de
Nat <nat@Nats-Mac-mini.local>
2021-11-28 17:35:13 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "pivotal-tower-332104"
+  region = "asia-southeast1"
+  zone = "asia-southeast1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
